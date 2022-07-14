3f9581b2e4ace989ccbcc91270d059e4a28988c5
Jim Davidson <jim@jimusik.com>
2022-07-13 18:41:21 -0700

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,47 @@
+#cxlogon installer
+new-module -name foobar -scriptblock {
+  $uri = "http://pub.nxfilter.org/cxlogon-win-1.0.4.msi"
+  $file = 'cxlogon-win-1.0.4.msi'
+  $temp = $Env:windir + "\Temp\"
+  $soft_name = 'cxlogon'
+  $logfile = $temp + "cxlogoninstaller.log"
+  $logdump = $temp + "cxlogondump.log"
+
+
+Function WriteLog {
+  Param ([string]$LogString)
+  $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+  $LogMessage = "$Stamp $LogString"
+  Add-content $logfile -value $LogMessage
+}
+
+Function Install-Project() {
+  param (
+    [string]$project = 'chef',
+    [string]$channel = 'stable'
+  )
+
+  #Start-Transcript -Append $logdump
+
+  $find = Get-WmiObject -Class Win32_Product -Filter "Name LIKE `'$soft_name`'"
+
+  if ($find -eq $null) {
+    $tmp = "$temp$file"
+    $client = New-Object System.Net.WebClient
+    $client.DownloadFile($uri, $tmp)
+
+    msiexec /i $tmp /qn
+    WriteLog "Tried installing $soft_name"
+  } else {
+    WriteLog "ERROR: $soft_name is already installed."
+    WriteLog $find
+
+  }
+  #Stop-Transcript
+
+  }
+
+  set-alias install -value Install-Project
+
+  export-modulemember -function 'WriteLog' -alias 'install'
+}
