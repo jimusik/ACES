3c311e7a8acdd6b0fd690cb0a5237f2a16f1929a
Jim Davidson <jim@jimusik.com>
2023-02-13 16:13:09 -0800

    Create screenconnect.ps1

---
+++ screenconnect.ps1
@@ -0,0 +1,32 @@
+#screenconnect installer
+new-module -name ScreenConnect -scriptblock {
+	$uri = "https://adv-public-package-space.sfo3.digitaloceanspaces.com/AdvConnectWiseControl.ClientSetup.exe"
+	$output = "AdvConnectWiseControl.ClientSetup.exe"
+	$soft_name = "ScreenConnect%"
+	$tmp = "c:\Windows\temp\"
+
+Function Install-Project {
+	
+		$find = Get-WmiObject -Class Win32_Product -Filter "Name LIKE `'$soft_name`'"
+
+		if ($find -eq $null) {
+		$tmp = "$temp$output"
+		$client = New-Object System.Net.WebClient
+		$client.DownloadFile($uri, $tmp)
+
+
+		# Install the EXE file
+		Start-Process $output -Wait
+	    Echo "Tried installing $soft_name"
+	  	} else {
+	    Echo "ERROR: $soft_name is already installed."
+	    Echo $find
+
+  }
+}
+  set-alias install -value Install-Project
+
+  export-modulemember -function 'Install-Project' -alias 'install'
+}
+
+
