fef072bc7a0fb3dd49b00f0380e434d33d1ea92f
Jim Davidson <jim@jimusik.com>
2023-02-13 16:30:01 -0800

    Update screenconnect.ps1

--- screenconnect.ps1
+++ screenconnect.ps1
@@ -1,7 +1,7 @@
 #screenconnect installer
 new-module -name ScreenConnect -scriptblock {
 	$uri = "https://adv-public-package-space.sfo3.digitaloceanspaces.com/AdvConnectWiseControl.ClientSetup.exe"
-	$output = "AdvConnectWiseControl.ClientSetup.exe"
+	$output = "ConnectWiseControl_ClientSetup.exe"
 	$soft_name = "ScreenConnect%"
 	$temp = "c:\Windows\temp\"
 
@@ -10,17 +10,17 @@ Function Install-Project {
 		$find = Get-WmiObject -Class Win32_Product -Filter "Name LIKE `'$soft_name`'"
 
 		if ($find -eq $null) {
-		$tmp = "$temp$output"
-		$client = New-Object System.Net.WebClient
-		$client.DownloadFile($uri, $tmp)
+			$tmp = "$temp$output"
+			$client = New-Object System.Net.WebClient
+			$client.DownloadFile($uri, $tmp)
 
 
-		# Install the EXE file
-		Start-Process $output -Wait
-	    Echo "Tried installing $soft_name"
+			# Install the EXE file
+			Start-Process $tmp -Wait
+	    	Echo "Tried installing $soft_name"
 	  	} else {
-	    Echo "ERROR: $soft_name is already installed."
-	    Echo $find
+	    	Echo "ERROR: $soft_name is already installed."
+	    	Echo $find
 
   			}
 		}
